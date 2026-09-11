ed12a370c8946a2adef499b296eaa0401e200b2a
kirakiralingo-1 <kirakiralingo@outlook.jp>
2026-09-11 15:16:49 +0900

    Update sw.js

--- public/sw.js
+++ public/sw.js
@@ -3,6 +3,14 @@ importScripts("/scram/scramjet.all.js");
 const { ScramjetServiceWorker } = $scramjetLoadWorker();
 const scramjet = new ScramjetServiceWorker();
 
+self.addEventListener("install", () => {
+  void self.skipWaiting();
+});
+
+self.addEventListener("activate", (event) => {
+  event.waitUntil(self.clients.claim());
+});
+
 self.addEventListener("fetch", (event) => {
   event.respondWith(async () => {
     await scramjet.loadConfig();
